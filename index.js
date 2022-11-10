eec1be420fa1513e2c17a4f207d05b89ab45dfb6
bimlendu33 <33177291+bimlendu33@users.noreply.github.com>
2022-11-10 10:45:00 +0530

    1 file modified

--- index.js
+++ index.js
@@ -10,17 +10,17 @@ appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
 console.log('Welcome to Programiz!');
 const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-var numRotations = 2;
-var rowArr=[]
+var numRotations = 4;
+var rotatedArray = [];
 
 function leftRotation(array, numRotations) {
   if (numRotations >= 1) {
     array = [...array.slice(1, array.length), array[0]];
     numRotations = numRotations - 1;
-    rowArr=[...array];
+    rotatedArray = [...array];
     leftRotation(array, numRotations);
   }
-  return rowArr;
+  return rotatedArray;
 }
 
-console.log(leftRotation(array, numRotations))
+console.log(leftRotation(array, numRotations));
